cfe0b0616a80db5ea4369c69eed5b2dcbaac04c2
Arvind Srinivas Subramanian <arvindsrinivas96@gmail.com>
2022-12-05 03:02:35 -0500

    dbchange

--- url_shortner_server/settingtest.py
+++ url_shortner_server/settingtest.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "url_shortner_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "urlshortner",
+        "USER": "djangouser",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
