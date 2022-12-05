3ec87b0bcb5db96273e65423b8cd85c0a238dca0
Arvind Srinivas Subramanian <arvindsrinivas96@gmail.com>
2022-12-05 04:22:54 -0500

    dblocalhost

--- url_shortner_server/url_shortner_server/settings.py
+++ url_shortner_server/url_shortner_server/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": "urlshortner",
         "USER": "djangouser",
         "PASSWORD": "password",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
