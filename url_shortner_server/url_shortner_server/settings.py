4f79268d0809e3ecbfde1ba375bf860aedeea8d0
Arvind Srinivas Subramanian <arvindsrinivas96@gmail.com>
2022-12-05 04:37:36 -0500

    mysqlsetup

--- url_shortner_server/url_shortner_server/settings.py
+++ url_shortner_server/url_shortner_server/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": "urlshortner",
         "USER": "djangouser",
         "PASSWORD": "password",
-        "HOST": "db",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
