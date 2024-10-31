3376a6bb7b21b2fd14bc6b9cc621e1837560c3af
Dhairya Shah <dhairyashah3000@gmail.com>
2024-10-31 16:02:15 -0400

    Added Home Page

--- url_shortner_server/shortner/views.py
+++ url_shortner_server/shortner/views.py
@@ -7,7 +7,7 @@ from shortner.update_view import UpdateView
 from shortner.custom_view import CustomView
 from shortner.stats_view import StatsView
 from shortner.login import login_test
-
+from django.shortcuts import render
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -94,6 +94,10 @@ def homepage(request, fname):
         args = {}
         args['userFname'] = fname
         return render(request, 'homepages/index.html', args)
+
+
+def about_us(request):
+    return render(request, 'homepages/AboutUs.html')  # Make sure the path matches your file's location
 
 
 def signout(request):
