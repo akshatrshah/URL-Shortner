6269c725d2763e86cde5c0ae3da8c1043aa51567
Akshat Shah <akshatshah@AkshatsLaptop5.lan>
2024-10-31 01:43:35 -0400

    process multiple comma separated URLs

--- url_shortner_server/shortner/new_view.py
+++ url_shortner_server/shortner/new_view.py
@@ -1,33 +1,45 @@
 """new_view module defines the NewView view"""
 
-from django.http.request import HttpRequest
+from django.http import HttpRequest, HttpResponseBadRequest
 from django.views.generic import View
 from django.shortcuts import redirect
-
 from shortner.models import Link
 
 
 class NewView(View):
-    """NewView is responsible for creation of new shortened links"""
+    """NewView is responsible for the creation of new shortened links"""
 
     def post(self, request: HttpRequest):
         """post handles post requests to /new"""
-        username = request.session['username']
-        if username == '':
-            redirect('signin')
-        long_url = ''
-        if 'long-url' in request.POST:
-            long_url = request.POST["long-url"]
-            custom_stub = None
-            if 'custom-stub' in request.POST and\
-                    request.POST['custom-stub'] != '':
-                custom_stub = request.POST["custom-stub"]
-        else:
-            redirect('homepage', fname=username)
-        link = Link(long_url)
-        link.username = username
-        if custom_stub is not None:
-            link.save_custom(custom_stub)
-        else:
-            link.save()
+        username = request.session.get('username')
+        if not username:
+            return redirect('signin')
+
+        if 'long-url' not in request.POST:
+            return redirect('homepage', fname=username)
+
+        long_urls = request.POST['long-url'].split(',')
+        custom_stubs = request.POST.get('custom-stub', '').split(',')  # Get custom stubs and split by comma
+
+        # Ensure custom_stubs is trimmed to match the long_urls length
+        custom_stubs = [stub.strip() for stub in custom_stubs]
+        custom_stubs = custom_stubs[:len(long_urls)]  # Trim the list to match long URLs
+
+        for index, long_url in enumerate(long_urls):
+            long_url = long_url.strip()
+            if not long_url:
+                continue
+
+            link = Link(long_url=long_url, username=username)
+
+            # Use custom_stub only for the corresponding URL
+            if index < len(custom_stubs) and custom_stubs[index]:
+                custom_stub = custom_stubs[index]
+                # Check if the custom stub already exists
+                if Link.objects.filter(stub=custom_stub).exists():
+                    return HttpResponseBadRequest(f"Custom stub '{custom_stub}' is already in use.")
+                link.save_custom(custom_stub)
+            else:
+                link.save()
+
         return redirect('list')
