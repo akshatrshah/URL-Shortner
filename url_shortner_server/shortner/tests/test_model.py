d206573c62420667745a2f90920726bfc7119e39
Akshat Shah <akshatshah@AkshatsLaptop5.lan>
2024-10-31 23:25:15 -0400

    test cases

--- url_shortner_server/shortner/tests/test_model.py
+++ url_shortner_server/shortner/tests/test_model.py
@@ -1,12 +1,18 @@
 from unittest import TestCase
 from shortner.models import Link
-
+import uuid
 
 class TestModel(TestCase):
     def test_create_new_entry(self):
+        # Generate a unique special_code to avoid duplicate entry error
+        unique_special_code = str(uuid.uuid4())
+
+        # Create a new Link instance
         link = Link.objects.create(
             long_url="www.ncsu.edu",
-            special_code="00000000-0000-0000-0000-000000000000",
+            special_code=unique_special_code,
             stub="aaaaaaaaaa",
         )
+
+        # Check if the instance is correctly created and to_json() returns a string
         self.assertIsInstance(link.to_json(), str)
--- url_shortner_server/shortner/tests/test_urls.py
+++ url_shortner_server/shortner/tests/test_urls.py
@@ -1,51 +1,88 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from shortner.views import DeleteView, NewView, UpdateView, StubView
-from shortner.views import StatsView, ListUrlsView, signup, signout, signin
-
+from shortner.views import (
+    DeleteView, NewView, UpdateView, StubView, StatsView, ListUrlsView,
+    signup, signout, signin, CustomView, home, delete_all_urls, about_us,
+    create_url, login_test, homepage
+)
 
 class TestUrls(SimpleTestCase):
     def test_add_url_is_resolved(self):
-        """Test adding url"""
         url = reverse('add_new')
         self.assertEqual(resolve(url).func.view_class, NewView)
 
     def test_delete_url_is_resolved(self):
-        """Test deleting url"""
         url = reverse('delete', args=['special'])
         self.assertEqual(resolve(url).func.view_class, DeleteView)
 
     def test_update_url_is_resolved(self):
-        """Test updating url"""
         url = reverse('update')
         self.assertEqual(resolve(url).func.view_class, UpdateView)
 
     def test_stub_url_is_resolved(self):
-        """Test stub url"""
         url = reverse('stub', args=['some-url'])
         self.assertEqual(resolve(url).func.view_class, StubView)
 
     def test_stats_url_is_resolved(self):
-        """Test stats url"""
         url = reverse('stats')
         self.assertEqual(resolve(url).func.view_class, StatsView)
 
     def test_list_url_is_resolved(self):
-        """Test list url"""
         url = reverse('list')
         self.assertEqual(resolve(url).func.view_class, ListUrlsView)
 
     def test_signin_url_is_resolved(self):
-        """Test signin url"""
         url = reverse('signin')
         self.assertEqual(resolve(url).func, signin)
 
     def test_signout_url_is_resolved(self):
-        """Test signout url"""
         url = reverse('signout')
         self.assertEqual(resolve(url).func, signout)
 
     def test_signup_url_is_resolved(self):
-        """Test signup url"""
         url = reverse('signup')
         self.assertEqual(resolve(url).func, signup)
+
+    def test_delete_all_url_is_resolved(self):
+        url = reverse('delete_all_urls')
+        self.assertEqual(resolve(url).func, delete_all_urls)
+
+    def test_export_csv_url_is_resolved(self):
+        url = reverse('export_csv')
+        self.assertEqual(resolve(url).func.view_class, StatsView)
+
+    def test_custom_url_is_resolved(self):
+        url = reverse('custom')
+        self.assertEqual(resolve(url).func.view_class, CustomView)
+
+    def test_home_url_is_resolved(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home)
+
+    def test_login_test_url_is_resolved(self):
+        url = reverse('Test')
+        self.assertEqual(resolve(url).func, login_test)
+
+    def test_about_us_url_is_resolved(self):
+        url = reverse('about_us')
+        self.assertEqual(resolve(url).func, about_us)
+
+    def test_create_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEqual(resolve(url).func, create_url)
+
+    def test_homepage_url_is_resolved(self):
+        url = reverse('homepage', args=['testuser'])
+        self.assertEqual(resolve(url).func, homepage)
+
+    def test_admin_url_is_resolved(self):
+        url = reverse('admin:index')
+        self.assertEqual(url, '/admin/')
+
+    def test_stats_export_csv_url_is_resolved(self):
+        url = reverse('export_csv')
+        self.assertEqual(resolve(url).func.view_class, StatsView)
+
+    # def test_nonexistent_url_raises_404(self):
+    #     with self.assertRaises(resolve.Resolver404):
+    #         resolve('/nonexistent-url/')
